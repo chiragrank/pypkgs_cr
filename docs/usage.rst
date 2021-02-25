cccee04daed274344bed6a8fdffe320f335ccc23
chirag <chiiragrank@gmail.com>
2021-02-25 10:56:58 -0500

    Initial setup of package

---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use pypkgs_cr in a project::
+
+    import pypkgs_cr
---
+++ tests/test_pypkgs_cr.py
@@ -0,0 +1,5 @@
+from pypkgs_cr import __version__
+from pypkgs_cr import pypkgs_cr
+
+def test_version():
+    assert __version__ == '0.1.0'
